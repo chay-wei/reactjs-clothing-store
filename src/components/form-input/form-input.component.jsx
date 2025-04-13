89388d47048d42f0600dc9f40a10bff4d9e08375
Chay Wei <chaywei.job@gmail.com>
2025-04-13 23:27:08 +0800

    modify prettier config, fix the code format, add button component, authentication component

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,12 @@
+import "./form-input.styles.scss"
+
+const FormInput = ({ label, otherProps }) => {
+  return (
+    <div className="group">
+      {label && <label className={`${otherProps.value.length ? "shrink" : ""} form-input-label`}>{label}</label>}
+      <input {...otherProps} className="form-input" />
+    </div>
+  )
+}
+
+export default FormInput
